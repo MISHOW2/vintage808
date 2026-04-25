64c32d0244637053b2f32b3f793ecc270e194b8e
MISHOW2 <mshibambo63@gmail.com>
2026-04-25 19:54:25 +0200

    fix payments cancel

--- client/js/pages/payment-processing.js
+++ client/js/pages/payment-processing.js
@@ -4,20 +4,21 @@ const params = new URLSearchParams(window.location.search);
 const orderId = params.get('order');
 
 async function pollPaymentStatus() {
+  console.log(new URLSearchParams(window.location.search).get('order'));
   try {
     const res = await fetch(`${API}/payfast/status/${orderId}`);
     const data = await res.json();
 
     if (!data.success) return;
 
-    if (data.order.status === 'paid') {
-      localStorage.removeItem('v808_cart');
-      window.location.href = `./order-confirmation.html?order=${orderId}`;
-    }
+   if (data.order.payment?.status === 'paid') {
+  localStorage.removeItem('v808_cart');
+  window.location.href = `./order-confirmation.html?order=${orderId}`;
+}
 
-    if (data.order.status === 'cancelled') {
-      window.location.href = './checkout.html?status=cancelled&restore=1';
-    }
+if (data.order.payment?.status === 'cancelled') {
+  window.location.href = './checkout.html?status=cancelled&restore=1';
+}
 
   } catch (err) {
     console.error(err);
